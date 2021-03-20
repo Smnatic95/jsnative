f03b3e88054847ad58c800c89cac5f0af861b282
你的用户名$ git config --global user.name 你的用户名^V <80231115+Smnatic95@users.noreply.github.com>
2021-03-20 21:23:35 +0800

    妹子主页

--- page/kefu/index.js
+++ page/kefu/index.js
@@ -12,17 +12,77 @@ $(function () {
         },
         spaceBetween: 20
     });
+    let serviceType,
+        timeType;
 
     $('.operation_bottom .chooseT').on('click', function () {
         mask.show();//显示遮罩
-        $('.xiaDanForm').css({
-            transform: 'translate(0,0)'
+        $('.xiaDanForm').addClass('out');
+    })
+
+    $('section.gg .types>.type').on('click', function () {
+        $(this).toggleClass('sel').siblings().removeClass('sel');
+        checkStatus();
+    })
+
+    $('.xiaDanForm .xiadanRightNow').on('click', function () {
+        let wechatNumber = $('.xiaDanForm section.wechat input').val();
+        if (!serviceType) {
+            mui.alert('请选择服务类型', '提示');
+        } else if (!timeType) {
+            mui.alert('请选择时长', '提示');
+        } else if (!wechatNumber){
+            mui.alert('请输入微信号', '提示');
+        }
+
+    })
+
+    function checkStatus() {
+        $('section.gg.service .types>.type').each((ind, item) => {
+            if ($(item).hasClass('sel')) {
+                serviceType = $(item).text();
+            }
         })
+        $('section.gg.time .types>.type').each((ind, item) => {
+            if ($(item).hasClass('sel')) {
+                timeType = $(item).text();
+            }
+        })
+        let xzStatusToa = '';
+        if (!serviceType && !timeType) {
+            xzStatusToa = '请选择服务类型,时长';
+        } else if (!serviceType) {
+            xzStatusToa = '请选择服务类型';
+        } else if (!timeType) {
+            xzStatusToa = '请选择时长';
+        } else {
+            xzStatusToa = `已选 ${serviceType},${timeType};`;
+        }
+
+        $('.xzStatusToa').text(xzStatusToa);
+    }
+
+    $('.operation .jian').on('click', function () {
+        let curNum = Number($(this).siblings('.curCount').text());
+        if (curNum <= 1) {
+            mui.toast('最少数量为1');
+        } else {
+            $(this).siblings('.curCount').text(curNum - 1);
+        }
+    })
+
+    $('.operation .add').on('click', function () {
+        let curNum = Number($(this).siblings('.curCount').text());
+        $(this).siblings('.curCount').text(curNum + 1);
+    })
+
+    $('.xiaDanForm>.close').on('click', function () {
+        mask.close();
+        $('.xiaDanForm').removeClass('out');
     })
 
     function clsMask() {
-        $('.xiaDanForm').css({
-            transform: 'translate(0,100%)'
-        })
+        $('.xiaDanForm').removeClass('out');
     }
+
 })
